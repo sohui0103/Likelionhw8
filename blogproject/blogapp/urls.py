e111edbd786a706370296e706e5d3fd8c4e5be28
jisoolee11 <jisu6414@likelion.org>
2021-06-25 13:30:36 +0900

    clonetest

---
+++ blogproject/blogapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('<int:id>/', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name='create'),
+    path('edit/<int:id>/', edit, name="edit"), 
+    path('update/<int:id>/', update, name="update"), 
+    path('delete/<int:id>/', delete, name="delete"),
+]
---
+++ blogproject/blogapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
+
+# Create your views here.
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs':blogs})
+    
+def detail(request, id):
+    details = get_object_or_404(Blog, pk=id)
+    return render(request, 'detail.html', {'details':details})
+
+# new.html을 띄워주는 함수
+def new(request): 
+    return render(request, 'new.html')
+
+ # 입력받은 내용을 데이터베이스에 넣어주는 함수
+def create(request):
+    blog = Blog()
+    blog.title = request.POST['title']
+    blog.body = request.POST['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
+
+def edit(request, id):
+    edit_blog = get_object_or_404(Blog, id=id)
+    return render(request, 'edit.html', {'blog':edit_blog})
+
+def update(request, id):
+    update_blog = get_object_or_404(Blog, id=id)
+    update_blog.title = request.POST['title']
+    update_blog.body = request.POST['body']
+    update_blog.pub_date = timezone.now()
+    update_blog.save()
+    return redirect('detail', str(update_blog.id))
+
+def delete(request, id):
+    delete_blog = get_object_or_404(Blog, id=id)
+    delete_blog.delete()
+    return redirect('home')
+
